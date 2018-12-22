b5f4b5653bc7e7a23104ed2c1cb8636c62e5cf1e
BorgVanDorren <32478281+borgvandorren@users.noreply.github.com>
2018-12-22 15:56:02 +0300

    Minor code formatting

--- app2_my_2.py
+++ app2_my_2.py
@@ -10,8 +10,8 @@ vol_df = pd.read_csv('volcanoes_rus_clear.csv')
 vol_df_a = pd.read_csv('volcanoes_world_active.csv')
 vol_df_a = vol_df_a.drop(['Country', 'Type'], 1)
 vol_df_a.columns = ['name', 'lat', 'lon', 'height']
-vol_df_a['lat'] = pd.to_numeric(vol_df_a['lat'], errors = 'coerse')
-vol_df_a['lon'] = pd.to_numeric(vol_df_a['lon'], errors = 'coerse')
+vol_df_a['lat'] = pd.to_numeric(vol_df_a['lat'], errors='coerse')
+vol_df_a['lon'] = pd.to_numeric(vol_df_a['lon'], errors='coerse')
 vol_df_a = vol_df_a.dropna()
 
 # Replaced by interrows iteration over DataFrame
@@ -36,6 +36,7 @@ def color_selector(el):
     else:
         return 'red'
 
+
 # Forming group of markers
 fgv = fl.FeatureGroup(name='Volcanoes')
 fga = fl.FeatureGroup(name='Active')
@@ -50,7 +51,8 @@ for i, row in vol_df.iterrows():
 for i, row in vol_df_a.iterrows():
 
     iframe = fl.IFrame(html=html % (row['name'], row['name'], row['height']), width=150, height=80)
-    fga.add_child(fl.Marker(location=[row['lat'], row['lon']], popup=fl.Popup(iframe), icon=fl.Icon(color=color_selector(row['height']))))
+    fga.add_child(fl.Marker(location=[row['lat'], row['lon']], popup=fl.Popup(iframe),
+                            icon=fl.Icon(color=color_selector(row['height']))))
 
 
 # Adding group to the map and saving the map
